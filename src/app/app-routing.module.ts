6bce84af7d12ec5fd28f9dc29e5f54f521068b3d
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-11-29 21:42:27 +0530

    nnew comp login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 import { AvinashComponent } from './avinash/avinash.component';
 
 const routes: Routes = [
   {
-    path: 'one',
+    path: 'avinash',
     component: AvinashComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
   },
  
 ];
